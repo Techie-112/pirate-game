123cb7a25fecea611fadff5c9137627e2395cf4c
Techie-112 <techiefixit112@gmail.com>
2025-02-22 21:07:30 +0000

    player rotation
    the player now faces whichever direction the mouse cursor is. intended for use with the shove feature

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,9 +5,16 @@ public class player : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
 
+    private SpriteRenderer cursprite;
+    [SerializeField] Sprite[] sprites;
+    //0 = back 1 = right 2 = left 3 = front
+
+    public Camera cam;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cursprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -15,10 +22,37 @@ public class player : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
+
+        facing();
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = moveInput * speed;
+    }
+
+    void facing()
+    {
+        //get the mouse position from the center of the screen
+        //this is because using input.mouseposition on its own
+        //returns a value using the bottom left corner of the scene as (0,0)
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        //get the distance between the player and the mouse
+        Vector2 dist = mousePos - transform.position;
+        
+        //use that distance to calculate the angle between the mouse and the player
+        //then convert into degrees
+        float angle = Mathf.Atan2(dist.y, dist.x)*Mathf.Rad2Deg;
+
+        if (angle >= 45 && angle <= 135)
+        { cursprite.sprite = sprites[0]; }
+        else if (angle >= -135 && angle <= -45)
+        { cursprite.sprite = sprites[2]; }
+        else if (angle <= 45 && angle >= -45)
+        { cursprite.sprite = sprites[1]; }
+        else if (angle >= 135 || angle <= -135)
+        { cursprite.sprite = sprites[3]; }
+
     }
 }
