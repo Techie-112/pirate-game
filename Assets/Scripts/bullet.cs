43f5b7d9a73e28f4b21740e38b5bf8b72828072d
Rocco156 <mrkingdice74@gmail.com>
2025-02-24 20:36:49 +0000

    added ranged enemies

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float muzzle_velocity = 10f;
+    private Rigidbody2D rgbd2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rgbd2D.linearVelocity = transform.TransformDirection(Vector3.right) * muzzle_velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Ranged_enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ranged_enemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class ranged_enemy : MonoBehaviour
+{
+    public Transform Player;
+    public float speed = 0.3f;
+    private Vector2 direction;
+    private Rigidbody2D rb2d;
+    private bool can_move = true;
+    public float targetTime = 120.0f;
+    public GameObject Bullet;
+    private float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb2d = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //get target position difference
+
+    }
+
+    private void FixedUpdate()
+    {
+        direction = (Player.position - transform.position).normalized;
+
+        angle = Mathf.Atan2(direction.x, direction.y);
+        //get angle between current rotation and targets position
+        Quaternion Target_rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        //rotate towards the target
+        transform.right = Player.position - transform.position;
+
+        targetTime -= Time.deltaTime;
+
+        if (can_move)
+        {
+            // move towards the player
+            rb2d.linearVelocity = new Vector2(direction.x * speed, direction.y * speed);
+        }
+
+
+        if (targetTime <= 0.00)
+        {
+            targetTime = 120f;
+            shoot();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(collision.gameObject);
+    }
+
+    public void shoot()
+    {
+        Debug.Log("Fired");
+        // create projectile with ranged enemies position and rotation 
+        Instantiate(Bullet, transform.position + (transform.right * 1.5f), transform.rotation);
+    }
+}
