677ef91f0b1a45a3333f1f72cb3a8cbdafe17f07
Techie112 <techiefixit112@gmail.com>
2025-02-25 13:45:20 +0000

    update health display
    quickly fixed the health display since it wasn't working last night

--- Assets/Scripts/healthDisplay.cs
+++ Assets/Scripts/healthDisplay.cs
@@ -3,28 +3,26 @@ using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
-    public int health;
-    public int maxHealth;
 
     public Sprite empty;
     public Sprite full;
     public Image[] hearts;
 
     public player player;
-    
+ 
+
+
     void Update()
     {
-        health = player.currentLives;
-        maxHealth = player.maxLives;
 
-        for (int i = 0; i < hearts.Length; i++)
+        for (int i = 0; i < player.maxLives; i++)
         {
-            if (i < health)
-            { hearts[i].sprite = full; }
+            if (i < player.currentLives)
+            { hearts[i].overrideSprite = full; }
             else
-            { hearts[i].sprite = empty; }
+            { hearts[i].overrideSprite = empty; }
 
-            if (i < maxHealth)
+            if (i < player.maxLives)
             { hearts[i].enabled = true; }
             else
             { hearts[i].enabled = false; }
