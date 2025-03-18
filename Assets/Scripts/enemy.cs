b1fade7d715b71bf6935a8862c9cfdf2c2775d77
SpiceLettuce <139508355+SpiceLettuce@users.noreply.github.com>
2025-03-18 02:43:27 +0000

    charge mechanic
    added a charge mechanic. it works but it still needs some stuff

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class enemy : MonoBehaviour
 {
@@ -7,12 +8,21 @@ public class enemy : MonoBehaviour
     public float rotationSpeed = 5f;
     public int damage = 1;
     player player;
-    
+    //charge mechanic variables
+    public float chargeForce = 500f;
+    public float stopDistance = 3f;
+    public float pauseTime = 1f;
+    public float chargeDrag = 8f; //drag doesn't work right now I'll fix it LATER!!!
+    public float chargeDuration;
+    private Vector2 chargeDirection;
+    private enum EnemyState { Approaching, Pausing, Charging }
+    private EnemyState currentState = EnemyState.Approaching;
+
     //variables related to enemy movement
     private Transform target;
     private Vector2 direction;
     private float angle;
-    private Rigidbody2D rb2d;
+    private Rigidbody2D rb;
 
     //variables related to enemy rotation
     private SpriteRenderer cursprite;
@@ -20,19 +30,36 @@ public class enemy : MonoBehaviour
     //0 = back 1 = right 2 = front 3 = left
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindWithTag("Player").transform;
         cursprite = GetComponent<SpriteRenderer>();
+        rb.linearDamping = 0;  // Ensure normal movement has no drag
     }
 
     void Update()
     {
-        direction = (target.position - transform.position).normalized;
+        
     }
 
     private void FixedUpdate()
     {
-        moveEnemy();
+        
+        if (currentState == EnemyState.Approaching)
+        {
+            ApproachPlayer();
+        }
+        else if (currentState == EnemyState.Pausing)
+        {
+            //this is just here for funsies I guess
+        }
+        else if ( currentState == EnemyState.Charging)
+        {
+            //Charge();
+        }
+        else
+        {
+            Debug.Log("currentState is somehow none of the above. It is: " +  currentState);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -55,7 +82,7 @@ public class enemy : MonoBehaviour
     }
 
 
-    private void moveEnemy()
+    private void ApproachPlayer()
     {
         /*this.angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
         //get angle between current rotation and target position
@@ -64,34 +91,66 @@ public class enemy : MonoBehaviour
         //rotate towards the target
         this.transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed); */
 
-        //move towards the player
-        this.rb2d.linearVelocity = new Vector2(direction.x * speed, direction.y * speed);
-        
-        //decide which way they are facing
-        if (rb2d.linearVelocityX > rb2d.linearVelocityY)
+        //get distance from player to this enemy
+        float distance = Vector2.Distance(transform.position, target.position);
+
+        if (distance > stopDistance)
         {
-            if (rb2d.linearVelocityX >= 0)
+            //move towards the player
+            direction = (target.position - transform.position).normalized;
+            rb.linearVelocity = direction * speed;
+
+            //decide which way this enemy is facing
+            if (rb.linearVelocityX > rb.linearVelocityY)
             {
-                //going right
-                cursprite.sprite = sprites[1];
-            } else
-            {
-                //going left
-                cursprite.sprite = sprites[3];
-            }
-        } else
-        {
-            if (rb2d.linearVelocityY >= 0)
-            {
-                //going up
-                cursprite.sprite = sprites[0];
+                if (rb.linearVelocityX >= 0)
+                {
+                    //going right
+                    cursprite.sprite = sprites[1];
+                }
+                else
+                {
+                    //going left
+                    cursprite.sprite = sprites[3];
+                }
             }
             else
             {
-                //going down
-                cursprite.sprite = sprites[2];
+                if (rb.linearVelocityY >= 0)
+                {
+                    //going up
+                    cursprite.sprite = sprites[0];
+                }
+                else
+                {
+                    //going down
+                    cursprite.sprite = sprites[2];
+                }
             }
-        }
 
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+            StartCoroutine(PauseBeforeCharge());
+        }
+    }
+    IEnumerator PauseBeforeCharge()
+    {
+        currentState = EnemyState.Pausing;
+        chargeDirection = (target.position - transform.position).normalized;
+        //gets direction BEFORE the pause, letting the player dodge
+        yield return new WaitForSeconds(pauseTime);
+
+        currentState = EnemyState.Charging;
+        rb.linearDamping = chargeDrag;  // Apply drag to slow down gradually
+        Debug.Log("chargeDirection: " + chargeDirection + "] wah [" + chargeForce);
+        rb.linearVelocity = chargeDirection * chargeForce;
+
+        yield return new WaitForSeconds(chargeDuration);  // Give time to charge
+        rb.linearDamping = 0;  // Reset drag for normal movement
+        currentState = EnemyState.Approaching;
+
+        //drag doesn't work right now I'll fix it LATER!!!
     }
 }
